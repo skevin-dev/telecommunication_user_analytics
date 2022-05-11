b1d7d3a38a2c39b65aa0c8c4668693fbda3eb183
Kevin <shyakakevin1@gmail.com>
2022-05-11 12:00:43 +0200

    file changed

--- data_clean.py
+++ data_clean.py
@@ -7,4 +7,9 @@ import matplotlib.pyplot as plt
 class clean_data():
     def __init__(self,df):
         self.df = df
-    
+        
+    def drop_high_nan_column(self,df):
+        column_30percent = [i for i in df.columns if df[i].isnull().sum()*100/len(df) >= 30 ]
+        df.drop(column_30percent,axis=1,inplace=True)
+        
+        return df 
