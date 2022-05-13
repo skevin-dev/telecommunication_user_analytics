836d7b5e77e7e3ae3160f16d53e45beb9c5d2189
Kevin <shyakakevin1@gmail.com>
2022-05-13 16:57:40 +0200

    file modified

--- scripts/normalization.py
+++ scripts/normalization.py
@@ -2,15 +2,13 @@ from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import MinMaxScaler
 
 class normalize_data():
-    def __init__(self,df):
-        self.df = df
-        
-    def normalizer(self,df):
+    
+    def normalizer(df):
         norm = Normalizer()
         normalized_data = norm.fit_transform(df)
         return normalized_data
 
-    def standardizer(self,df):
+    def standardizer(df):
         scaler = MinMaxScaler()
         scaled_data = scaler.fit_transform(df)
         return scaled_data
